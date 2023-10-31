59caf176c0746762ec844614087e8e3db5724095
Txulia04 <115453736+Txulia04@users.noreply.github.com>
2023-10-31 13:34:03 +0100

    Lección 3 completada

--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
@@ -160,6 +160,20 @@ namespace WindowsFormsApplication1
 
         }
 
-     
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Pedir numero de servicios
+            string mensaje = "7/";
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+            contLbl.Text = mensaje;
+        }
     }
 }
